9c3dfdad8d554778c3d966ead9b6c9d6c04d9fad
pensadotech <armando@pensadotech.com>
2018-11-08 21:21:46 -0800

    adding index route

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ const bodyparser = require('body-parser')
 const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(bodyparser.urlencoded({extended: true}))
+app.use(bodyparser.urlencoded({
+  extended: true
+}))
 app.use(bodyparser.json())
 
-// require('./routes')(app)
+require('./routes')(app)
 
 // require('./models').sequelize.sync().then(() => {
 //     app.listen(2000, () => console.log('http://localhost:2000'))
 // })
 
-app.listen(2000, () => console.log('http://localhost:2000'))
+// app.listen(2000, () => console.log('http://localhost:2000'))
+
+// sync DB and listen
+require('./models').sequelize.sync().then(() => {
+  const PORT = process.env.PORT || 2000
+  app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`)
+  })
+})
