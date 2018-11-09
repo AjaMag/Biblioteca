8f3d29fd578735f7a7f20e045dc256c8a7156f1b
Phoenix <aja.magdaleno@gmail.com>
2018-11-08 19:03:00 -0800

    with gitignore and all folders

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const db = require('../models')
+
+module.exports = app => {
+    app.get('/ ', (req, res) => {
+        db. .findAll({})
+        .then(r => res.json(r))
+        .catch(e => console.log(e))
+    })
+
+    app.get('/ ', (req, res) => {
+        db. .findAll({})
+        .then(r => res.json(r))
+        .catch(e => console.log(e))
+    })
+    app.post('/ ', (req, res) => {
+        console.log(req.body)
+        db. .create(req.body)
+        .then(r => res.sendStatus(200))
+        .catch(e => console.log(e))
+    })
+
+    app.post('/ ', (req, res) => {
+        db. .create(req.body)
+        .then(r => res.sendStatus(200))
+        .catch(e => console.log(e))
+    })
+}
