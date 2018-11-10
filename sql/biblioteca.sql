d8a8cd57b75a9ef65e58b86772f216a10995fcb3
pensadotech <armando@pensadotech.com>
2018-11-10 11:54:07 -0800

    adding inster sql statement for sample sections and books

--- sql/biblioteca.sql
+++ sql/biblioteca.sql
@@ -11,3 +11,20 @@ drop table carts;
 use biblioteca_db;
 drop table books;
 drop table sections;
+
+-- inserting sampple data
+
+-- add data
+USE biblioteca_db;
+INSERT INTO sections (location,description,createdAt,updatedAt)
+VALUE("1A","Children Books",now(),now()),
+("1B","Children Magazine",now(),now()),
+("2A","Biology",now(),now()),
+("3A","Physics",now(),now());
+
+USE biblioteca_db;
+INSERT INTO books (title,author,year,genre,ISBN,summary,stockQty,totalUse,createdAt,updatedAt,sectionId)
+VALUE("Book-1","auhtor-1",2018,"genre-1","tre123","book sumarry 1",10,4,now(),now(),1),
+("Book-2","auhtor-2",2018,"genre-1","rew123","book sumarry 1",10,4,now(),now(),1),
+("Book-3","auhtor-3",2018,"genre-2","abc123","book sumarry 1",10,4,now(),now(),3),
+("Book-4","auhtor-4",2018,"genre-2","xwy123","book sumarry 1",10,4,now(),now(),4);
